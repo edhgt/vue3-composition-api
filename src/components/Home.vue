913ebd4fd95f9e3ac34c0f232b82f2ab90f961d6
Danilo Hernandez <dhernandez0032@gmail.com>
2022-03-29 11:58:57 -0600

    computed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,56 +1,23 @@
 <template>
     <div>
-        {{ text }}
-        {{ counter }}
+        {{ fullName }}
     </div>
 </template>
 
 <script>
-import { ref, reactive, watch } from 'vue';
+import { ref, computed } from 'vue';
 
 export default {
     setup() {
-        /*
-        Para tipos primitivos: string, number, bigint, boolean, undefined, symbol
-        */
-        const text = ref("Hola Vue");
-        const counter = ref(0);
+        const firstName = ref('Danilo');
+        const lastName = ref('H');
 
-        // leer variable
-        text.value
-
-        // asignar
-        text.value = "Que hay de nuevo viejo"
-
-        setInterval(() => counter.value++, 500)
-
-        /*
-        Para objetos literales
-        */
-        const obj = reactive({ counter: 0 })
-        obj.counter
-        obj.counter++
-
-        setInterval(() => obj.counter++, 500)
-
-        /*
-        Si queremos escuchar el cambio del objeto entero
-        */
-        watch(obj, (valor, anterior) => {
-            console.log(valor, anterior)
-        })
-
-        /*
-        Si queremos escuchar el cambio en una propiedad
-        */
-        watch(() => obj.counter, (valor, anterior) => {
-            console.log(valor, anterior)
-        })
+        const fullName = computed(() => {
+            return `${firstName.value} ${lastName.value}`
+        });
 
         return {
-            text,
-            counter,
-            obj
+            fullName
         }
     }
 }
