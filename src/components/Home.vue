97b023a220a18c475a30b1104410b179be111693
Danilo Hernandez <dhernandez0032@gmail.com>
2022-03-29 09:37:40 -0600

    ciclo de vida en composition api

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Hola
+    </div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+
+export default {
+    setup() {
+        onMounted(() => {
+            console.log("onMounted")
+        })
+    }
+}
+</script>
