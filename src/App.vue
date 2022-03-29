6128de6779c43e68cdf8f26c8d3d1255f51df684
Danilo Hernandez <dhernandez0032@gmail.com>
2022-03-29 08:30:11 -0600

    ciclo de vida

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,36 @@
 <template>
-  <div>
-    <Modal />
-  </div>
+    <div>
+        {{ text }}
+    </div>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
-
 export default {
-  name: "App",
-  components: {
-    Modal,
-  },
-  data() {
-    return {
-      show: false
+    name: "App",
+    data() {
+        return {
+            text: "Hola Vue"
+        };
+    },
+    beforeCreate() {
+        console.log("beforeCreate", this.$data, this.$el)
+    },
+    created() {
+        console.log("created", this.$data, this.$el)
+    },
+    mounted() {
+        console.log("mounted", this.$data, this.$el)
     }
-  }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.fade.enter.from
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-leave-active,
-.fade-enter-active {
-  transition: opacity 0.5s ease;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
